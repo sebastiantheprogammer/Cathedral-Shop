5e0c4e0354eeec61190579b5b3114b2d1befc3df
sebastiantheprogammer <sebastianingram121510@gmail.com>
2025-04-07 22:48:02 -0400

    Updated server.js with admin password and CORS headers

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.use(express.json());
 app.use(express.static('public'));
 app.set('trust proxy', true);
 
+// Add CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.post('/submit', (req, res) => {
   const userIP = req.ip;
   const answer = req.body.answer;
